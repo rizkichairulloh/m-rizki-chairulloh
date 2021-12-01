3413f4d4df15b31d7c4d4b007c20bbe253dd54be
rizkichairulloh <rizkim47@gmail.com>
2021-12-01 19:21:08 +0700

    remove meta description

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -8,9 +8,9 @@ export default function ContainerBlock({ children, ...customMeta }) {
   const router = useRouter();
 
   const meta = {
-    title: "Muh Rizki Chairullo - Mobile Apps Developer",
+    title: "Muh Rizki Chairulloh - Mobile Apps Developer",
     description: `I've been developing websites for 5 years straight. Get in touch with me to know more.`,
-    image: "/avatar.png",
+    image: "/avatar.jpeg",
     type: "website",
     ...customMeta,
   };
@@ -30,11 +30,11 @@ export default function ContainerBlock({ children, ...customMeta }) {
         />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Muh Rizki Chairulloh" />
-        <meta property="og:description" content={meta.description} />
+        {/* <meta property="og:description" content={meta.description} /> */}
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
+        <meta name="twitter:site" content="@rizkichairulloh" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
